1178953421d6976832add284355dd4811c46660d
mstiller7 <mstiller@ggc.edu>
2019-07-19 15:32:53 -0500

    Progress...

--- applied-quaternions.py
+++ applied-quaternions.py
@@ -1,15 +1,21 @@
 import open_myo as myo
 import numpy as np
+import timeit
+import time
+import pickle
 
 EMG = list()
 IMU = list()
+
+get_reading = False
 
 
 def processEMG(emg):
     global EMG
     # print emg
     EMG = emg
-    processData()
+    if get_reading:
+        processData()
 
 
 def processIMU(quat, acc, gyro):
@@ -19,43 +25,73 @@ def processIMU(quat, acc, gyro):
 
 
 def processData():
-    global EMG, IMU
+    global EMG, IMU, gestures, NAME
     e = np.array(EMG)
     i = np.array(IMU)
     i.resize(e.shape)
-    print np.matmul(e, i)
+    dot_product = np.matmul(e, i)
+    print(dot_product)
+    gestures[name][i].append[dot_product]
 
 
-def main():
-    print('Connecting to Myo armband...')
-    # assign the device to a var. get the MAC address first!
-    myo_mac_addr = myo.get_myo()
-    myo_device = myo.Device()
+gestures = dict()
+NAME = ''
 
-    # print developer information to console.
-    print("MAC: %s" % myo_mac_addr)
-    fw = myo_device.services.firmware()
-    print("Firmware: %d.%d.%d.%d" % (fw[0], fw[1], fw[2], fw[3]))
-    print("Battery: %d" % myo_device.services.battery())
+print('Connecting to Myo armband...')
+# assign the device to a var. get the MAC address first!
+myo_mac_addr = myo.get_myo()
+myo_device = myo.Device()
 
-    # never sleep.
-    myo_device.services.sleep_mode(1)
-    # short vibration.
-    myo_device.services.vibrate(1)
+# print developer information to console.
+print("MAC: %s" % myo_mac_addr)
+fw = myo_device.services.firmware()
+print("Firmware: %d.%d.%d.%d" % (fw[0], fw[1], fw[2], fw[3]))
+print("Battery: %d" % myo_device.services.battery())
 
-    myo_device.services.emg_filt_notifications()
-    myo_device.services.imu_notifications()
-    myo_device.services.set_mode(
-        myo.EmgMode.FILT, myo.ImuMode.DATA, myo.ClassifierMode.OFF)
+# never sleep.
+myo_device.services.sleep_mode(1)
+# short vibration.
+myo_device.services.vibrate(1)
 
-    myo_device.add_emg_event_handler(processEMG)
-    myo_device.add_imu_event_handler(processIMU)
+myo_device.services.emg_filt_notifications()
+myo_device.services.imu_notifications()
+myo_device.services.set_mode(
+    myo.EmgMode.FILT, myo.ImuMode.DATA, myo.ClassifierMode.OFF)
 
-    # main program loop. await service notifications.
-    while True:
-        if myo_device.services.waitForNotifications(1):
-            continue
-        print("Waiting...")
+myo_device.add_emg_event_handler(processEMG)
+myo_device.add_imu_event_handler(processIMU)
 
 
-main()
+n_gestures = int(
+    raw_input("How many gestures do you want to perform? (2<x<11): "))
+n_iterations = int(raw_input(
+    "How many times do you want to repeat each gesture? (x>1, default:3): "))
+runtime = int(
+    raw_input("How many seconds do you want each gesture to last (default: 2): "))
+
+
+for g in range(n_gestures):
+    name = raw_input("Enter the name of gesture number {}: ".format(g+1))
+    # NAME = name
+    gestures[name] = list()
+    for i in range(n_iterations):
+        gestures[name].append(list())
+        raw_input("Iteration {}. Press enter to begin recording.".format(i+1))
+        myo_device.services.vibrate(1)  # short vibration
+        time.sleep(2)
+    #        myo_device.services.set_mode(myo.EmgMode.RAW, myo.ImuMode.OFF, myo.ClassifierMode.OFF)
+        start_time = timeit.default_timer()
+        tick = start_time
+        get_reading = True
+        while round(tick - start_time, 1) <= runtime:
+            if myo_device.services.waitForNotifications(1):
+                tick = timeit.default_timer()
+    #                continue
+            else:
+                print("Waiting...")
+    #        myo_device.services.set_mode(myo.EmgMode.OFF, myo.ImuMode.OFF, myo.ClassifierMode.OFF)
+        get_reading = False
+        myo_device.services.vibrate(1)  # short vibration
+
+
+# main()
--- emg_save-signals.py
+++ emg_save-signals.py
@@ -10,58 +10,66 @@ import pickle
 
 get_reading = False
 
+
 def process_emg(emg):
     if get_reading:
-	gestures[name][i].append(emg)
-	# gestures[name][i].append(emg[1])
+        gestures[name][i].append(emg)
+        # gestures[name][i].append(emg[1])
 #        emg_values.append(emg[0])
 #        emg_values.append(emg[1])
+
 
 def save_data(data):
     timestr = time.strftime("%Y%m%d-%H%M%S")
     name = raw_input('Save file as: ')
     filename = "emg-data_"+timestr+"-"+name
     with open("emg-data/"+filename+".pkl", 'wb') as fp:
-	pickle.dump(data, fp)
+        pickle.dump(data, fp)
+
 
 myo_device = myo.Device()
-myo_device.services.sleep_mode(1) # never sleep
-myo_device.services.set_leds([128, 128, 255], [128, 128, 255])  # purple logo and bar LEDs)
-myo_device.services.vibrate(1) # short vibration
+myo_device.services.sleep_mode(1)  # never sleep
+# purple logo and bar LEDs)
+myo_device.services.set_leds([128, 128, 255], [128, 128, 255])
+myo_device.services.vibrate(1)  # short vibration
 myo_device.services.emg_filt_notifications()
 # myo_device.services.emg_raw_notifications()
 # myo_device.services.set_mode(myo.EmgMode.RAW, myo.ImuMode.OFF, myo.ClassifierMode.OFF)
-myo_device.services.set_mode(myo.EmgMode.FILT, myo.ImuMode.OFF, myo.ClassifierMode.OFF)
+myo_device.services.set_mode(
+    myo.EmgMode.FILT, myo.ImuMode.OFF, myo.ClassifierMode.OFF)
 time.sleep(1)
 myo_device.add_emg_event_handler(process_emg)
 
 gestures = dict()
-n_gestures = int(raw_input("How many gestures do you want to perform? (2<x<11): "))
-n_iterations = int(raw_input("How many times do you want to repeat each gesture? (x>1, default:3): "))
-runtime = int(raw_input("How many seconds do you want each gesture to last (default: 2): "))
+n_gestures = int(
+    raw_input("How many gestures do you want to perform? (2<x<11): "))
+n_iterations = int(raw_input(
+    "How many times do you want to repeat each gesture? (x>1, default:3): "))
+runtime = int(
+    raw_input("How many seconds do you want each gesture to last (default: 2): "))
 
 for g in range(n_gestures):
     name = raw_input("Enter the name of gesture number {}: ".format(g+1))
     gestures[name] = list()
     for i in range(n_iterations):
-	gestures[name].append(list())
-	raw_input("Iteration {}. Press enter to begin recording.".format(i+1))
-	myo_device.services.vibrate(1) # short vibration
-	time.sleep(2)
+        gestures[name].append(list())
+        raw_input("Iteration {}. Press enter to begin recording.".format(i+1))
+        myo_device.services.vibrate(1)  # short vibration
+        time.sleep(2)
 #        myo_device.services.set_mode(myo.EmgMode.RAW, myo.ImuMode.OFF, myo.ClassifierMode.OFF)
-	start_time = timeit.default_timer()
-	tick = start_time
-	get_reading = True
-	while round(tick - start_time, 1) <= runtime:
-	    if myo_device.services.waitForNotifications(1):
-		tick = timeit.default_timer()
+        start_time = timeit.default_timer()
+        tick = start_time
+        get_reading = True
+        while round(tick - start_time, 1) <= runtime:
+            if myo_device.services.waitForNotifications(1):
+                tick = timeit.default_timer()
 #                continue
-	    else:
-		print("Waiting...")
+            else:
+                print("Waiting...")
 #        myo_device.services.set_mode(myo.EmgMode.OFF, myo.ImuMode.OFF, myo.ClassifierMode.OFF)
-	get_reading = False
-	myo_device.services.vibrate(1) # short vibration
+        get_reading = False
+        myo_device.services.vibrate(1)  # short vibration
 
 save_data(gestures)
-# print(tick - start_time)   
+# print(tick - start_time)
 print('Saved data.')
