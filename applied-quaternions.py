c456dcf53980ac9fe156369b1a3e65f6b5b5b46c
mstiller7 <mstiller@ggc.edu>
2019-07-24 16:12:32 -0500

    lijdsfdkdiusoflj.kmfsdh

--- applied-quaternions.py
+++ applied-quaternions.py
@@ -5,20 +5,25 @@ import time
 emgs = list()
 imus = list()
 
+doProcessing = False
+
 def processEMG(emg):
-    del emgs[:]
-    emgs.append(emg)
+    if doProcessing:
+        # del emgs[:]
+        emgs.append(emg)
 
 
 def processIMU(quat, acc, gyro):
-    del imus[:]
-    imus.append(quat)
+    if doProcessing:
+        # del imus[:]
+        imus.append(quat)
 
 
 def getDotProduct(emg, imu):
     e = np.array(emg)
     i = np.array(imu)
-    i.resize(e.shape)
+    i = np.resize(i, e.shape)
+    # i.resize(e.shape)
     dp = np.matmul(e, i)
     print dp
     return dp
@@ -27,34 +32,34 @@ def getDotProduct(emg, imu):
 gestures = dict()
 
 
-def connectBT():
-    print('Connecting to Myo armband...')
-    # assign the device to a var. get the MAC address first!
-    myo_mac_addr = myo.get_myo()
-    myo_device = myo.Device()
+# def connectBT():
+print('Connecting to Myo armband...')
+# assign the device to a var. get the MAC address first!
+myo_mac_addr = myo.get_myo()
+myo_device = myo.Device()
 
-    # print developer information to console.
-    print("MAC: %s" % myo_mac_addr)
-    fw = myo_device.services.firmware()
-    print("Firmware: %d.%d.%d.%d" % (fw[0], fw[1], fw[2], fw[3]))
-    print("Battery: %d" % myo_device.services.battery())
+# print developer information to console.
+print("MAC: %s" % myo_mac_addr)
+fw = myo_device.services.firmware()
+print("Firmware: %d.%d.%d.%d" % (fw[0], fw[1], fw[2], fw[3]))
+print("Battery: %d" % myo_device.services.battery())
 
-    # never sleep.
-    myo_device.services.sleep_mode(1)
-    # short vibration.
-    myo_device.services.vibrate(1)
+# never sleep.
+myo_device.services.sleep_mode(1)
+# short vibration.
+myo_device.services.vibrate(1)
 
-    myo_device.services.emg_filt_notifications()
-    myo_device.services.imu_notifications()
-    myo_device.services.set_mode(
-        myo.EmgMode.FILT, myo.ImuMode.DATA, myo.ClassifierMode.OFF)
+myo_device.services.emg_filt_notifications()
+myo_device.services.imu_notifications()
+myo_device.services.set_mode(
+    myo.EmgMode.FILT, myo.ImuMode.DATA, myo.ClassifierMode.OFF)
 
-    myo_device.add_emg_event_handler(processEMG)
-    myo_device.add_imu_event_handler(processIMU)
+myo_device.add_emg_event_handler(processEMG)
+myo_device.add_imu_event_handler(processIMU)
 
-    return myo_device
+# return myo_device
 
-MYO = connectBT()
+# MYO = connectBT()
 
 doLoop = True
 while doLoop:
@@ -62,18 +67,19 @@ while doLoop:
     gestures[name] = list()
     raw_input("Press enter to begin recording.")
 
-    MYO.services.vibrate(1)
-    # doProcessing = True
+    myo_device.services.vibrate(1)
+    doProcessing = True
 
     # for 3 seconds...
-    end = time.time() + 3
-    while time.time() < end:
+    # end = time.time() + 3
+    # while time.time() < end:
+    for x in range(3):
         e = emgs[-1]
         i = imus[-1]
         gestures[name].append(getDotProduct(e, i))
 
-    # doProcessing = False
-    MYO.services.vibrate(1)
+    doProcessing = False
+    myo_device.services.vibrate(1)
 
     choice = raw_input("Complete. Do another? [y/n] ")
     if choice == 'y':
